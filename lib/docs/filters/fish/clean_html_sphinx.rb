248f62be8ea0fc31763ba8041d076eb095942963
bryan <bryan.guner@gmail.com>
2021-08-18 13:21:28 -0400

    initial commit

---
+++ lib/docs/filters/fish/clean_html_sphinx.rb
@@ -0,0 +1,13 @@
+module Docs
+  class Fish
+    class CleanHtmlSphinxFilter < Filter
+      def call
+        @doc = at_css('.body')
+        css('pre[data-language="fish"]').each do |node|
+          node['data-language'] = 'shell'
+        end
+        doc
+      end
+    end
+  end
+end
